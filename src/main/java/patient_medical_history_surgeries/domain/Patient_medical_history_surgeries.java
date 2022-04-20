584e87316273ca8321727f256b0d3420d4dbcda2
amulya12 <30744874+amulya12@users.noreply.github.com>
2022-04-19 20:18:39 -0500

    Updated domain file

--- src/main/java/patient_medical_history_surgeries/domain/Patient_medical_history_surgeries.java
+++ src/main/java/patient_medical_history_surgeries/domain/Patient_medical_history_surgeries.java
@@ -1,23 +1,19 @@
-package patient_medical_history.domain;
+package patient_medical_history_surgeries.domain;
 
 import java.sql.Date;
 
 /**
  * User object
  * 
- * @author Aayush Makharia
+ * @author AB
  * 
  */
-public class Patient_medical_history {
+public class Patient_medical_history_surgeries {
 	/*
 	 * Correspond to the user table
 	 */
 	private Integer history_id;
-	private String conditions;
-	private Date last_visit;
-	private Integer patient_id;
-	
-	
+	private Integer surgeries;
 	
 	public Integer getHistory_id() {
 		return history_id;
@@ -25,30 +21,21 @@ public class Patient_medical_history {
 	public void setHistory_id(Integer history_id) {
 		this.history_id = history_id;
 	}
-	public String getConditions() {
-		return conditions;
+	public Integer getSurgeries() {
+		return surgeries;
 	}
-	public void setConditions(String conditions) {
-		this.conditions = conditions;
+	public void setSurgeries(Integer surgeries) {
+		this.surgeries = surgeries;
 	}
-	public Date getLast_visit() {
-		return last_visit;
-	}
-	public void setLast_visit(Date last_visit) {
-		this.last_visit = last_visit;
-	}
-	public Integer getPatient_id() {
-		return patient_id;
-	}
-	public void setPatient_id(Integer patient_id) {
-		this.patient_id = patient_id;
-	}
-	
 	@Override
 	public String toString() {
-		return "Patient_medical_history [history_id=" + history_id + ", conditions=" + conditions + ", last_visit="
-				+ last_visit + ", patient_id=" + patient_id + "]";
+		return "Patient_medical_history_surgeries [history_id=" + history_id + ", surgeries=" + surgeries + "]";
 	}
+	
+	
+	
+	
+	
 	
 	
 		
