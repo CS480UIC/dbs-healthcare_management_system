b219062e5199983559f929f3893d74b20147d74b
amulya12 <30744874+amulya12@users.noreply.github.com>
2022-04-19 20:15:58 -0500

    Added Patient_medical_history_surgeries

---
+++ src/main/java/patient_medical_history_surgeries/service/Patient_medical_history_surgeriesException.java
@@ -0,0 +1,18 @@
+package patient_medical_history.service;
+
+public class Patient_medical_historyException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public Patient_medical_historyException() {
+		super();
+	}
+
+	public Patient_medical_historyException(String message) {
+		super(message);
+	}
+
+}
