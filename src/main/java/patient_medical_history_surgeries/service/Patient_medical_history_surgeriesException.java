accb8a21ca0e28f9f6ac5a456ce8ba80369226f4
amulya12 <30744874+amulya12@users.noreply.github.com>
2022-04-19 20:32:03 -0500

    Updated

--- src/main/java/patient_medical_history_surgeries/service/Patient_medical_history_surgeriesException.java
+++ src/main/java/patient_medical_history_surgeries/service/Patient_medical_history_surgeriesException.java
@@ -1,17 +1,17 @@
-package patient_medical_history.service;
+package patient_medical_history_surgeries.service;
 
-public class Patient_medical_historyException extends Exception {
+public class Patient_medical_history_surgeriesException extends Exception {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public Patient_medical_historyException() {
+	public Patient_medical_history_surgeriesException() {
 		super();
 	}
 
-	public Patient_medical_historyException(String message) {
+	public Patient_medical_history_surgeriesException(String message) {
 		super(message);
 	}
 
