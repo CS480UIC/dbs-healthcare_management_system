47f5752c97eef37aa58b5a792cee791297418b9f
amulya12 <30744874+amulya12@users.noreply.github.com>
2022-04-20 14:32:09 -0500

    Updated service in Patient_medical_history_surgeries

--- src/main/java/patient_medical_history_surgeries/service/Patient_medical_history_surgeriesService.java
+++ src/main/java/patient_medical_history_surgeries/service/Patient_medical_history_surgeriesService.java
@@ -22,8 +22,8 @@ public class Patient_medical_history_surgeriesService {
 	public void create(Patient_medical_history_surgeries form) throws Patient_medical_history_surgeriesException, ClassNotFoundException, InstantiationException, IllegalAccessException{
 		// check the primary key of Entity1
 		//System.out.println(form);
-		Patient_medical_history_surgeries patient_medical_history_surgeries = patient_medical_history_surgeriesDao.findByMID(form.getHistory_id());
-		if(patient_medical_history_surgeries.getHistory_id() != null && patient_medical_history_surgeries.getHistory_id()==form.getHistory_id()) throw new Patient_medical_history_surgeriesException("This patient_medical_history_surgeries name has been registered!");
+		Patient_medical_history_surgeries patient_medical_history_surgeries = patient_medical_history_surgeriesDao.findByMID(form.getHistory_id(), form.getSurgeries());
+		if((patient_medical_history_surgeries.getHistory_id() != null && patient_medical_history_surgeries.getHistory_id()==form.getHistory_id()) && (patient_medical_history_surgeries.getSurgeries()!=null && patient_medical_history_surgeries.getSurgeries()== form.getSurgeries()) ) throw new Patient_medical_history_surgeriesException("This patient_medical_history_surgeries name has been registered!");
 		patient_medical_history_surgeriesDao.add(form);
 	}
 	
